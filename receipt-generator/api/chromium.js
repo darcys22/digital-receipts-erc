2416d004973cfb46440a89df1d5a3c657c6cd404
Sean Darcy <darcys22@gmail.com>
2022-08-21 14:36:39 +1000

    changes the chromium so it always loads the local binary, also dynamically finds path

--- receipt-generator/api/chromium.js
+++ receipt-generator/api/chromium.js
@@ -1,13 +1,13 @@
 const chrome = require( "chrome-aws-lambda" );
 const puppeteer = require( "puppeteer-core" );
 
+let PUPPETEER_REVISIONS = import("puppeteer/lib/esm/puppeteer/revisions.js");
+
 async function getScreenshot ( html ) {
   //var exePath = "./node_modules/puppeteer/.local-chromium/mac-722234/chrome-win/chrome.exe"; //uncomment this line when working on localhost Windows
   //var exePath = "./node_modules/puppeteer/.local-chromium/mac-901912/chrome-mac/Chromium.app/Contents/MacOS/Chromium"; //uncomment this line when working on localhost Mac
-  var exePath = "./node_modules/puppeteer/.local-chromium/linux-982053/chrome-linux/chrome"; //uncomment this line when working on localhost Linux
-  if (process.env.NODE_ENV == 'production') {
-    exePath = await chrome.executablePath
-  }
+  const revision_obj = JSON.parse(JSON.stringify(await PUPPETEER_REVISIONS));
+  var exePath = "./node_modules/puppeteer/.local-chromium/linux-" + revision_obj.PUPPETEER_REVISIONS.chromium + "/chrome-linux/chrome";
   const browser = await puppeteer.launch({
     args: chrome.args,
     headless: chrome.headless, // comment this line when working on localhost
